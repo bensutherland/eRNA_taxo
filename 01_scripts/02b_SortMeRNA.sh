0e465b86029b4ac35c0c4fc44309a4a538875646
bensutherland <bensutherland7@gmail.com>
2017-07-12 11:39:23 -0700

    Remove spaces from SortMeRNA for loop to run

--- 01_scripts/02b_SortMeRNA.sh
+++ 01_scripts/02b_SortMeRNA.sh
@@ -2,28 +2,19 @@
 # Index the SortMeRNA databases in the main SortMeRNA directory 
 
 # Global variables
-TRIMMED_FOLDER="04_SortMeRNA_test"
+TRIMMED_FOLDER="03_trimmed"
 SORTMERNA_DB="/home/ben/Programs/sortmerna-2.1-linux-64"
 
 # User set variable
-NUM_CPU="10"
+NUM_CPU="12"
 
 
 # Running SortMeRNA
-ls -1 $TRIMMED_FOLDER/*_ilvd_trimmed.fq |
+ls -1 $TRIMMED_FOLDER/S13*_ilvd_trimmed.fq |
     sort -u |
     while read i
     do
         echo "Treating $i"
-        sortmerna --ref \
-        $SORTMERNA_DB/rRNA_databases/silva-bac-16s-id90.fasta,$SORTMERNA_DB/index/silva-bac-16s-db:\
-        $SORTMERNA_DB/rRNA_databases/silva-bac-23s-id98.fasta,$SORTMERNA_DB/index/silva-bac-23s-db:\
-        $SORTMERNA_DB/rRNA_databases/silva-arc-16s-id95.fasta,$SORTMERNA_DB/index/silva-arc-16s-db:\
-        $SORTMERNA_DB/rRNA_databases/silva-arc-23s-id98.fasta,$SORTMERNA_DB/index/silva-arc-23s-db:\
-        $SORTMERNA_DB/rRNA_databases/silva-euk-18s-id95.fasta,$SORTMERNA_DB/index/silva-euk-18s-db:\
-        $SORTMERNA_DB/rRNA_databases/silva-euk-28s-id98.fasta,$SORTMERNA_DB/index/silva-euk-28s:\
-        $SORTMERNA_DB/rRNA_databases/rfam-5s-database-id98.fasta,$SORTMERNA_DB/index/rfam-5s-db:\
-        $SORTMERNA_DB/rRNA_databases/rfam-5.8s-database-id98.fasta,$SORTMERNA_DB/index/rfam-5.8s-db\
-        --reads $i --sam --num_alignments 1 --fastx --aligned "$i"_rRNA --other "$i"_non_rRNA \
-        --log -v -a $NUM_CPU
+        sortmerna --ref $SORTMERNA_DB/rRNA_databases/silva-bac-16s-id90.fasta,$SORTMERNA_DB/index/silva-bac-16s-db:$SORTMERNA_DB/rRNA_databases/silva-bac-23s-id98.fasta,$SORTMERNA_DB/index/silva-bac-23s-db:$SORTMERNA_DB/rRNA_databases/silva-arc-16s-id95.fasta,$SORTMERNA_DB/index/silva-arc-16s-db:$SORTMERNA_DB/rRNA_databases/silva-arc-23s-id98.fasta,$SORTMERNA_DB/index/silva-arc-23s-db:$SORTMERNA_DB/rRNA_databases/silva-euk-18s-id95.fasta,$SORTMERNA_DB/index/silva-euk-18s-db:$SORTMERNA_DB/rRNA_databases/silva-euk-28s-id98.fasta,$SORTMERNA_DB/index/silva-euk-28s:$SORTMERNA_DB/rRNA_databases/rfam-5s-database-id98.fasta,$SORTMERNA_DB/index/rfam-5s-db:$SORTMERNA_DB/rRNA_databases/rfam-5.8s-database-id98.fasta,$SORTMERNA_DB/index/rfam-5.8s-db \
+        --reads $i --sam --num_alignments 1 --fastx --aligned "$i"_rRNA --other "$i"_non_rRNA --log -v -a $NUM_CPU -m 4096 --paired_in
     done
