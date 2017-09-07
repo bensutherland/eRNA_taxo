8350f49690bb699f104f7caf31a1c7fb8764ce61
bensutherland <bensutherland7@gmail.com>
2017-09-07 15:12:05 -0700

    Reduce by one step the samtools stages

--- 01_scripts/04_bowtie2.sh
+++ 01_scripts/04_bowtie2.sh
@@ -7,11 +7,12 @@ TRIMMED_FOLDER="04_sorted"
 MAPPED_FOLDER="07_mapped"
 
 # Choose reference
-REFERENCE="06_metatranscriptome/assemblies_merged_bbmap_reduced"
+#REFERENCE="06_metatranscriptome/assemblies_merged_bbmap_reduced"
 #REFERENCE="06_metatranscriptome/two_libs_contig"
+REFERENCE="06_metatranscriptome/assemblies_merged_red0.95_bbmap_red.fa"
 
 # User variables
-NUM_THREADS="6"
+NUM_THREADS="2"
 
 # Map reads and add RG
 ls -1 $TRIMMED_FOLDER/*non_rRNA.fq |
@@ -22,19 +23,20 @@ ls -1 $TRIMMED_FOLDER/*non_rRNA.fq |
 	  name=$(basename $i)
 	  label=$(echo $name | perl -pe 's/\_L001\_ilvd\_trimmed\.fq\_non\_rRNA\.fq//')
 	  ID="@RG\tID:${label}\tSM:${label}\tPL:Illumina"
-	  #bowtie2 --end-to-end -k 40 --threads $NUM_THREADS --rg-id $ID -x $REFERENCE --interleaved $i -S $i.bowtie2.sam
+	  bowtie2 --end-to-end -k 40 --threads $NUM_THREADS --rg-id $ID -x $REFERENCE --interleaved $i -S $i.bowtie2.sam
+          ##no longer needed?
 	  # samtools view -Sb $i.bowtie2.sam > $i.bowtie2.unsorted.bam
           
           ## With sufficient filtering flags (-q 2 -F 4) #mapping qual > 2
-          samtools view -b -F 4 -q 2 $i.bowtie2.unsorted.bam > $i.bowtie2.mapped.unsorted.bam
+          samtools view -Sb -F 4 -q 2 $i.bowtie2.sam > $i.bowtie2.unsorted.bam
           
-          # new 'mapped name' one 
-	  samtools sort -n $i.bowtie2.mapped.unsorted.bam -o $i.sorted.bam #samtools >= 1.5 
+          # Sort 
+	  samtools sort -n $i.bowtie2.unsorted.bam -o $i.sorted.bam #samtools >= 1.5 
 
           ## In case using samtools <1.5
 	  # samtools sort -n $i.bowtie2.unsorted.bam $i.bowtie2.sorted #samtools < 1.5
 
-          #rm $i.bowtie2.sam
+          rm $i.bowtie2.sam
 done
 
 # clean up space
