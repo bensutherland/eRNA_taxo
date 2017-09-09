ee4f87b7f5e007c28877fe7a5da86cf16c30efc6
bensutherland <bensutherland7@gmail.com>
2017-09-09 14:17:25 -0700

    Set up export contig names R

--- 01_scripts/07_edgeR_norm.R
+++ 01_scripts/07_edgeR_norm.R
@@ -26,11 +26,13 @@ names(interp)
 my.counts <- read.csv(file = "08_gx_levels/out.matrix.csv")
 
 # Import data
-dim(my.counts) #total unique tags = 4,528,162
+dim(my.counts) 
+##bbmap_only: total unique tags =    4528162
+##cdhit95_bbmap: total unique tags = 4081958 
 
 ## Backup stuff
 # my.counts.bk <- my.counts # just in case
-# my.counts <- my.counts.bk # go backwarks
+# my.counts <- my.counts.bk # CAUTION go backwarks
 
 # Set up DGEList
 rownames(my.counts) <- my.counts[,1]
@@ -52,6 +54,8 @@ min.ind <- 5 # choose the minimum number of individuals that need to pass the th
 # identify tags passing filter
 keep <- rowSums(cpm(my.counts)>cpm.filt) >= min.ind # Find which transcripts pass the filter
 table(keep) # gives number passing, number failing
+##bbmap_only: total retained (TRUE) =    
+##cdhit95_bbmap: total retained (TRUE) = 20488 
 
 # subset DGEList
 my.counts <- my.counts[keep, , keep.lib.sizes=FALSE] #keep.lib.sizes = T retains original lib sizes, otherwise recomputes w remaining tags
@@ -67,8 +71,12 @@ plot(my.counts$samples$norm.factors ~ my.counts$samples$lib.size)
 
 #### 3. Create design matrix ####
 # Use approximated variables in bins
-binary.pCO2 <- interp$Range[interp$file.name!="S13"] # currently missing one
-binary.season <- interp$season[interp$file.name!="S13"] # currently missing one
+binary.pCO2 <- interp$Range 
+binary.season <- interp$season
+
+#missing s13
+## binary.pCO2 <- interp$Range[interp$file.name!="S13"] # currently missing one
+## binary.season <- interp$season[interp$file.name!="S13"] # currently missing one
 
 # Build a design matrix
 #designMat <- model.matrix(~binary.pCO2)
@@ -91,25 +99,6 @@ summary(my.counts$prior.df) # est. overall var. across genome for dataset
 sqrt(my.counts$common.disp) #coeff of var, for biol. var
 
 plotBCV(my.counts)
-
-
-# #### 4. Prepare Output ####
-# # generate CPM matrix
-# normalized.output <- cpm(my.counts, normalized.lib.sizes = TRUE, log= F)
-# 
-# # Compare the raw counts to the normalized cpm values (not log)
-# my.counts$counts[1:5, 1:5] # not normalized, raw counts
-# normalized.output[1:5, 1:5] # normalized lib size calculated cpm values
-# 
-# # # output as normalized linear
-# # write.csv(normalized.output, file = "03_normalized_data/normalized_output_matrix.csv")
-# # 
-# # # # output as normalized log2 (in progress)
-# # normalized.output.log2 <- cpm(my.counts, normalized.lib.sizes = TRUE, log= T, prior.count = 1)
-# # write.csv(normalized.output, file = "03_normalized_data/normalized_output_matrix_log2.csv")
-# # 
-# # # output object
-# # save.image(file = "02_input_data/sfon_wgcna_01_output.RData") # save out existing data 
 
 
 #### 5. Visualize data ####
@@ -140,7 +129,7 @@ lrt.coef3 <- glmLRT(fit, coef = 3) # season
 lrt.coef4 <- glmLRT(fit, coef = 4) # pCO2 x season (effect of pCO2 depends on the season)
 
 # Set lrt of choice
-LOC <- lrt.coef2
+LOC <- lrt.coef4
 dim(topTags(LOC, p.value=0.05, n = 15000)) # how many genes DE w/ adj. p-val < 0.05
 
 # # Obtain result
@@ -168,3 +157,26 @@ write.csv(x = output, file = "output.csv")
 
 # FINAL FIXES
 # need to incorporate the seasonality factor into the DE model
+
+# Export only expressed genes:
+expr.contigs <- dimnames(lrt)[[1]]
+write.table(x = expr.contigs, file = "expr_contigs.csv" , quote = F, sep = ","
+          , row.names = F, col.names = FALSE)
+
+# #### 4. Prepare Output ####
+# # generate CPM matrix
+# normalized.output <- cpm(my.counts, normalized.lib.sizes = TRUE, log= F)
+# 
+# # Compare the raw counts to the normalized cpm values (not log)
+# my.counts$counts[1:5, 1:5] # not normalized, raw counts
+# normalized.output[1:5, 1:5] # normalized lib size calculated cpm values
+# 
+# # # output as normalized linear
+# # write.csv(normalized.output, file = "03_normalized_data/normalized_output_matrix.csv")
+# # 
+# # # # output as normalized log2 (in progress)
+# # normalized.output.log2 <- cpm(my.counts, normalized.lib.sizes = TRUE, log= T, prior.count = 1)
+# # write.csv(normalized.output, file = "03_normalized_data/normalized_output_matrix_log2.csv")
+# # 
+# # # output object
+# # save.image(file = "02_input_data/sfon_wgcna_01_output.RData") # save out existing data 
