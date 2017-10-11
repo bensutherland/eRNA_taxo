6b231dc4ade192d4489d1239004c3e02a90886ee
bensutherland <bensutherland7@gmail.com>
2017-10-11 14:32:52 -0700

    Minor cleanup of alignment script

--- 01_scripts/04_bowtie2.sh
+++ 01_scripts/04_bowtie2.sh
@@ -7,12 +7,10 @@ TRIMMED_FOLDER="04_sorted"
 MAPPED_FOLDER="07_mapped"
 
 # Choose reference
-#REFERENCE="06_metatranscriptome/assemblies_merged_bbmap_reduced"
-#REFERENCE="06_metatranscriptome/two_libs_contig"
-REFERENCE="06_metatranscriptome/assemblies_merged_red0.95_bbmap_red.fa"
+REFERENCE="06_metatranscriptome/16_libs_contig.fa"
 
 # User variables
-NUM_THREADS="2"
+NUM_THREADS="6"
 
 # Map reads and add RG
 ls -1 $TRIMMED_FOLDER/*non_rRNA.fq |
@@ -20,14 +18,16 @@ ls -1 $TRIMMED_FOLDER/*non_rRNA.fq |
 	while read i
 	do
 	  echo $i
+
+          # Create name
 	  name=$(basename $i)
 	  label=$(echo $name | perl -pe 's/\_L001\_ilvd\_trimmed\.fq\_non\_rRNA\.fq//')
 	  ID="@RG\tID:${label}\tSM:${label}\tPL:Illumina"
+
+	  # Align
 	  bowtie2 --end-to-end -k 40 --threads $NUM_THREADS --rg-id $ID -x $REFERENCE --interleaved $i -S $i.bowtie2.sam
-          ##no longer needed?
-	  # samtools view -Sb $i.bowtie2.sam > $i.bowtie2.unsorted.bam
           
-          ## With sufficient filtering flags (-q 2 -F 4) #mapping qual > 2
+          ## With filt flags (-q 2 -F 4), i.e. map qual > 2 ; -F 4 (no unmapped reads)
           samtools view -Sb -F 4 -q 2 $i.bowtie2.sam > $i.bowtie2.unsorted.bam
           
           # Sort 
